7d7c5de925849d05f9cb9ed3fd420641e844a014
Luis Cruz <luiscruz@luiss-mac-mini.local>
2018-06-30 20:24:00 -0300

    Initial Commit

---
+++ eureka/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eureka
+//
+//  Created by Luís Eduardo S C Cruz on 30/06/18.
+//  Copyright © 2018 Luís Eduardo S C Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
